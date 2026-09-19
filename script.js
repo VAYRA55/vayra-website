5b64798eb5104808472b154f05e35eb4a6e2a33b
VAYRA55 <wearvayra@gmail.com>
2026-09-19 17:27:18 +0530

    Fix image source for VAYRA ORIGIN product

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function renderProducts(filter="all"){
     el.className="product-card";
 
     const image = p.name.includes("ORIGIN")
-      file_00000000d31c8208b91f62dc8c6c846f.png
+            ? '<img src="file_00000000d31c8208b91f62dc8c6c846f.png" alt="VAYRA ORIGIN Oversized T-Shirt">'
       : '<div class="product-visual"><div class="tee"><span>VAYRA</span></div></div>';
 
     el.innerHTML=`
